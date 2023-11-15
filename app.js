f5388bdd816e97ed42680afea106d4d1b2ab43f9
Maksym Melnyk <maksym.melnyk@gamesys.com>
2023-11-15 11:03:44 +0200

    111

--- app.js
+++ app.js
@@ -3,27 +3,30 @@ const fs = require('fs');
 const port = 3000;
 
 const responseMappings = {
-  '/api/mockdata': 'data.json',
-  '/kyc-module/v1.0/configs/s4tvvonz740203/value?configName=AUTO_KYC_VALIDATION': {"value":true},
-  '/kyc-module/v1.0/configs/s4tvvonz740203/value?configName=AUTO_KYC_VERIFICATION': {"value":true},
-  '/kyc-module/verification/v1.2/players/s4tvvonz740203': {"data":{"code":0}},
-  '/kyc-module/v1.1/kycdocverification/s4tvvonz740203/verificationType' : '/mocks/kyc-module-verificationType.json',
-  '/kyc-module/fsbo/history?playerToken=s4tvvonz740203&page=0&size=100' : "/mocks/kycDocumentsHistory.json",
-  '/kyc-module/fsbo/requestableDocuments?playerToken=s4tvvonz740203' : "/mocks/kyc-module-requestableDocuments.json",
-  '/kyc-module/fsbo/requestableDocument?playerToken=s4tvvonz740203&id=6' : "/mocks/kyc-module-requestableDocument_6.json",
-  '/kyc-module/fsbo/requestableDocument?playerToken=s4tvvonz740203&id=1' : "/mocks/kyc-module-requestableDocument_1.json",
-  '/player-module/player/read?playertoken=s4tvvonz740203': "/mocks/player-module__player.json",
-  '/player-module/v2/players?q=s4tvvonz740203' : "/mocks/player-module-v2-players.json"
+  'GET:/api/mockdata': '/mocks/data.json',
+  'GET:/kyc-module/v1.0/configs/s4tvvonz740203/value?configName=AUTO_KYC_VALIDATION': {"value":true},
+  'GET:/kyc-module/v1.0/configs/s4tvvonz740203/value?configName=AUTO_KYC_VERIFICATION': {"value":true},
+  'GET:/kyc-module/verification/v1.2/players/s4tvvonz740203': {"data":{"code":0}},
+  'POST:/kyc-module/verification/v1.2/players/s4tvvonz740203': {"data": {"code": 1, "description": "E-KYC Light"}},
+  'GET:/kyc-module/verification/v1.2/statuses': '/mocks/verificationStatuses.json',
+  'GET:/kyc-module/v1.1/kycdocverification/s4tvvonz740203/verificationType' : '/mocks/kyc-module-verificationType.json',
+  'GET:/kyc-module/fsbo/history?playerToken=s4tvvonz740203&page=0&size=100' : "/mocks/kycDocumentsHistory.json",
+  'GET:/kyc-module/fsbo/requestableDocuments?playerToken=s4tvvonz740203' : "/mocks/kyc-module-requestableDocuments.json",
+  'GET:/kyc-module/fsbo/requestableDocument?playerToken=s4tvvonz740203&id=6' : "/mocks/kyc-module-requestableDocument_6.json",
+  'GET:/kyc-module/fsbo/requestableDocument?playerToken=s4tvvonz740203&id=1' : "/mocks/kyc-module-requestableDocument_1.json",
+  'POST:/player-module/player/read?playertoken=s4tvvonz740203': "/mocks/player-module__player.json",
+  'GET:/player-module/v2/players?q=s4tvvonz740203' : "/mocks/player-module-v2-players.json"
 };
 
 const server = http.createServer((req, res) => {
   try {
     const urlPath = req.url;
+    const requestMethod = req.method;
 
-    console.log(`${getCurrentTime()} - ${urlPath}`);
+    console.log(`${getCurrentTime()} - ${requestMethod} ${urlPath}`);
 
-    if (urlPath in responseMappings) {
-      const response = responseMappings[urlPath];
+    if (`${req.method}:${urlPath}` in responseMappings) {
+      const response = responseMappings[`${req.method}:${urlPath}`];
 
       res.writeHead(200, {
         'Content-Type': 'application/json',
